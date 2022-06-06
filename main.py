8e52c55ae137c58a041710443d9308fabb334326
Zaiden13 <louan.leplae@gmail.com>
2022-06-06 17:08:51 +0200

    fix:call all functions

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import colorama
 from etl import check_students, check_campus_staff, check_intervenant, check_modules
 from queries import add_students, add_staff, add_intervenants, add_modules
 
-
 colorama.init(autoreset=True)
-
 
 if __name__ == "__main__":
 	add_students(check_students())
-	# check_campus_staff()
-	# check_intervenant()
-	# check_modules()
+	add_staff(check_campus_staff())
+	add_intervenants(check_intervenant())
+	add_modules(check_modules())
 
